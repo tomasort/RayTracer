ab20987fb8715313d1946126ac5487dc34462205
tomasort <tomasortega.cs@gmail.com>
2021-10-18 14:53:22 -0400

    first commit

---
+++ cmake-build-debug/CMakeFiles/assignment2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment2.dir/src/main.cpp.o"
+  "assignment2"
+  "assignment2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assignment2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
